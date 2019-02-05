1439eb4ad56e1b6d74bb1c50c5ced68871bfaf70
Roberto Romero <creativeworks27@icloud.com>
2019-02-05 03:15:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(arrayNames){
   arrayNames = ["Doc", "Dopey", "Bashful","Grumpy"];
-  for (i = 0; i < cars.length; i++) { 
+  for (i = 0; i < arrayNames.length; i++) { 
   text += cars[i] + "<br>";
 }
 
